dcd81b1332435884dd8b43c3f3a5e1c1b1f1fe66
Monica <macanyiz@gmail.com>
2023-04-26 13:46:10 +0200

    He hecho modificaciones solo falta el apartado de ventas Signed-off-by: Monica <macanyiz@gmail.com>

--- tema8ejercicio5/src/tema8ejercicio5/ConnectionSingleton.java
+++ tema8ejercicio5/src/tema8ejercicio5/ConnectionSingleton.java
@@ -1,4 +1,4 @@
-package tema8ejercicio2;
+package tema8ejercicio5;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,7 +8,7 @@ public class ConnectionSingleton {
 	private static Connection con;
 
 	public static Connection getConnection() throws SQLException {
-		String url = "jdbc:mysql://127.0.0.1:3307/Provincia";
+		String url = "jdbc:mysql://127.0.0.1:3307/tienda";
 		String user = "alumno";
 		String password = "alumno";
 		if (con == null || con.isClosed()) {
